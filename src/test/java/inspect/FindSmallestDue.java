f7c8e69dd1906e1d377d54011cf0553aea2e989c
hanhtien02 <hanhtien02@gmail.com>
2019-03-19 11:45:44 +0700

    Print out last name of person who have smallest Due. I did the name refactor

--- src/test/java/inspect/FindSmallestDue.java
+++ src/test/java/inspect/FindSmallestDue.java
@@ -18,43 +18,34 @@ public class FindSmallestDue {
         // open a link by chrome
         openBrowser(BrowserName.Chrome);
         navigate("https://the-internet.herokuapp.com/tables");
-
         // calculate number of rows
         List<WebElement> rows = getElements(How.XPATH, "//table[@id = 'table1']/tbody/tr");
-
         // calculate number of columns
         List<WebElement> columns = getElements(How.XPATH, "//table[@id = 'table1']/thead/tr/th");
-
         // index of last name
-        int indexOfColumnLastName = findIndexOfColumn("Last Name", columns.size());
+        int columnLastNameIdx = findIndexOfColumn("Last Name", columns.size());
         // index of due
-        int indexOfColumnDue = findIndexOfColumn("Due", columns.size());
+        int columnDueIdx = findIndexOfColumn("Due", columns.size());
 
         // find and print last name of person who have smallest due
-
-        Double minOfDue = findMinOfDue(rows.size(), indexOfColumnDue);
-
+        Double minOfDue = findMinOfDue(rows.size(), columnDueIdx);
 
         // print last name of person who have smallest Due
-
         // create a Array to save list smallest Due
-        List<String> smallestDuePersons = new ArrayList<String>();
+        List<String> smallestDueList = new ArrayList<String>();
 
         // search in the table
-        for (int row = 1; row <= rows.size(); row++) {
+        for (int rowIdx = 1; rowIdx <= rows.size(); rowIdx++) {
             // get value of a cell in Due column in double to compare
-            Double value = parseCellToDouble(row, indexOfColumnDue);
-
+            Double cellValue = parseCellToDouble(rowIdx, columnDueIdx);
 
             // compare with minOfDue and save the last name
-            if (value.equals(minOfDue)) {
-                smallestDuePersons.add(getCell(row,indexOfColumnLastName));
+            if (cellValue.equals(minOfDue)) {
+                smallestDueList.add(getCell(rowIdx,columnLastNameIdx));
             }
         }
         //Print out the smallest list
-        System.out.println("Last name of person who have SMALLEST Due are(is): " + smallestDuePersons);
-
-
+        System.out.println("Last name of person who have SMALLEST Due are(is): " + smallestDueList);
         close();
     }
 
@@ -63,39 +54,34 @@ public class FindSmallestDue {
         return getElement(How.XPATH,
                 String.format(cellLocator, rowIndex, columnIndex)).getText();
     }
-
     // get the index of column based on column size
     public static int findIndexOfColumn(String columnName, int columnSize) {
-        int indexOfColumn = 0;// -1 in case not found
-        for (int column = 1; column <= columnSize; column++) {//columnIdx
+        int indexOfColumn = -1;// -1 in case not found
+        for (int columnIdx = 1; columnIdx <= columnSize; columnIdx++) {//columnIdx
             String cellValue = getElement(How.XPATH,
-                    String.format("//table[@id = 'table1']/thead/tr/th[%d]", column))
+                    String.format("//table[@id = 'table1']/thead/tr/th[%d]", columnIdx))
                     .getText();
 
             if (cellValue.equalsIgnoreCase(columnName)) {
-                indexOfColumn = column;
+                indexOfColumn = columnIdx;
             }
         }
         return indexOfColumn;
     }
-
     // format a cell value to Double
     public static double parseCellToDouble(int rowIndex, int columnIndex) {
         String cellToDouble = getCell(rowIndex, columnIndex).replaceAll("\\$", "");
 
         return Double.parseDouble(cellToDouble);
-
     }
-
-
     //Find the minimum value
-    public static double findMinOfDue(int rowSize, int indexOfDue) {
+    public static double findMinOfDue(int rowSize, int dueIdx) {
         // min value is the first value of the DUE column
-        double minOfDue = parseCellToDouble(1, indexOfDue);
+        double minOfDue = parseCellToDouble(1, dueIdx);
 
         // find the minimum
-        for (int row = 1; row <= rowSize; row++) {
-            double cellValue = parseCellToDouble(row, indexOfDue);
+        for (int rowIdx = 1; rowIdx <= rowSize; rowIdx++) {
+            double cellValue = parseCellToDouble(rowIdx, dueIdx);
             if (cellValue < minOfDue) {
                 minOfDue = cellValue;
             }
