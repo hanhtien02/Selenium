23b5f09f6df598e1c978170bee880a8de61584ea
hanhtien02 <hanhtien02@gmail.com>
2019-03-23 09:57:10 +0700

    Print out last name of person who have Due = $50

--- src/test/java/inspect/NetFrame.java
+++ src/test/java/inspect/NetFrame.java
@@ -1,5 +1,19 @@
 package inspect;
 
+import Support.BrowserName;
+import org.openqa.selenium.support.How;
+
+import static Support.ComonFunctions.*;
+
 public class NetFrame {
+    public static void main(String[] args) throws InterruptedException {
+        openBrowser(BrowserName.Chrome);
+        visit("https://the-internet.herokuapp.com/nested_frames");
+        //getDriver().switchTo().frame("frame-bottom");
+        getDriver().switchTo().frame("frame-top");//
+        getDriver().switchTo().frame("frame-left");
+        System.out.println(getText(How.XPATH, "/html/body"));
+        close();
+    }
 
 }
